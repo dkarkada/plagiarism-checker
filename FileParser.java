0da926819f033c7c1d26ac8c9ec98cfea4806189
Gina Chen <ginachrox@gmail.com>
2017-10-08 12:49:06 -0500

    make file parsing method

--- FileParser.java
+++ FileParser.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class FileParser{
     private File document;
@@ -27,11 +29,17 @@ public class FileParser{
     }
 
     private static String deleteQuotes(String s){
-        //goes through string and deletes quotation marks and apostrophes
+        String withoutQuotes = s.replace("'","");//goes through string and deletes apostrophes
+        withoutQuotes = s.replace("\"", "");//goes through string and deletes quotation marks
+        withoutQuotes = s.replace("-","");//goes through string and deletes hyphens
+        return withoutQuotes;
     }
 
     private ArrayList<String> parse(String s){
+        String line = deleteQuotes(s);
+        String[] parsedLine;
         ArrayList<String> answer = new ArrayList<String>();
+        parsedLine = line.split("\\P{L}+");//splits string whenever it hits something that isn't a letter
         //write a parsing method
         return answer;
     }
